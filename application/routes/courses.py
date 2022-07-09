400907b568a493bb39663717ba3cd2fe8671f54d
y001003 <y001003@naver.com>
2022-07-09 16:11:53 +0900

    feat: DL predict function added in flask module

--- application/routes/courses.py
+++ application/routes/courses.py
@@ -1,14 +1,14 @@
 from flask import Flask, Blueprint, render_template
 from flask import request
 # import tensorflow as tf
-# from tensorflow.keras import datasets, utils
-# from tensorflow.keras import models, layers, activations, initializers, losses, optimizers, metrics
+from tensorflow.keras import datasets, utils
+from tensorflow.keras import models, layers, activations, initializers, losses, optimizers, metrics
 
 import pandas as pd
 import numpy as np
 
-import jinja2
-from pycaret.regression import *
+# import jinja2
+# from pycaret.regression import *
 
 bp = Blueprint('courses',__name__,url_prefix='/')
 
@@ -26,17 +26,33 @@ def PredictionSpecialSale():
     ftype = request.args.get('ftype')
     odometer = request.args.get('odometer')
     
-    Input = pd.DataFrame({
-        'model':[vmodel],
-        'option':[voption],
-        'year':[vyear],
-        'fuel':[ftype],
-        'odometer':[int(odometer)],
-        'car':[vname],
-    })
-    loaded_model = load_model('final_AutoML_model')
+    ## pycaret result
+    # Input = pd.DataFrame({
+    #     'model':[vmodel],
+    #     'option':[voption],
+    #     'year':[vyear],
+    #     'fuel':[ftype],
+    #     'odometer':[int(odometer)],
+    #     'car':[vname],
+    # })
+    # loaded_model = load_model('final_AutoML_model')
 
-    ModelOutput = loaded_model.predict(Input)
+    # ModelOutput = loaded_model.predict(Input)
+
+    ## DL result
+    train_df = pd.DataFrame(pd.read_csv('.../data/train_df.csv')).iloc[:,1:]
+    cols = list(train_df.drop('price',axis=1).columns)
+
+    temp = [0] * 1104
+    odometer_MinMax = (odometer - train_df['odometer'].min(axis=0)) / (train_df['odometer'].max(axis=0) - train_df['odometer'].min(axis=0))
+    temp[0] = odometer_MinMax
+    temp[cols.index('car_'+vname)] =1
+    temp[cols.index('model_'+vmodel)] =1
+    temp[cols.index('option_'+voption)] =1
+    temp[cols.index('year_'+vyear)] =1
+    temp[cols.index('fuel_'+ftype)] =1
     
+    reconstructed_model = models.load_model("DL_model")
+    predicted = reconstructed_model.predict(np.array(temp).reshape(1,-1))
 
-    return render_template('courses.html', Output = ModelOutput)
+    return render_template('courses.html', Output = predicted)
